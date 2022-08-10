800f814d779ecb7ab53ee9e323a2ec32582d41c0
GUSTAVO MOCELIN PROCOPIO <gustavo.9942@aluno.pr.senac.br>
2022-08-09 22:23:03 -0300

    subindo framework

--- bootstrap/Env.php
+++ bootstrap/Env.php
@@ -1,11 +1,24 @@
 <?php
 
-class env{
+class env
+{
 
-    public function __construct(){
+    public static function execute()
+    {
+        $contentOfEnvFile = file_get_contents(__DIR__ . "\..\.env");
+        $arrayEnv = explode("\n", $contentOfEnvFile);
 
+        foreach ($arrayEnv as $value) {
+            $keyAndValue = explode("=", $value);
 
+            if (!isset($keyAndValue[1])) {
+                continue;
+            }
+
+            $nameOfVariable = $keyAndValue[0];
+            $valueOfVariable = $keyAndValue[1];
+
+            $_ENV[$nameOfVariable] = $valueOfVariable;
+        }
     }
-
-
 }
---
+++ helper/helper.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+function dd($input){
+var_dump($input);
+die();
+}
+
+function env($nameOfVariable){
+    return $_ENV[$nameOfVariable];
+}
+
