06e6285748ee1cf2cb0237da96a0f059b633352a
GUSTAVO MOCELIN PROCOPIO <gustavo.9942@aluno.pr.senac.br>
2022-08-23 22:18:42 -0300

    aula mostrar api no front

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 
 $mainPosition = __DIR__;//posição diretorio/repositorio
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 require_once("{$mainPosition}\helper\helper.php");
 require_once("{$mainPosition}\\vendor\autoload.php"); //sistema inclusão classe
 
 use Bootstrap\Env;
 use App\FrameworkTools\ProcessServerElements;
 use App\FrameworkTools\Implementations\FactoryMethods\FactoryProcessServerElement;
+use App\FrameworkTools\Implementations\Route\RouteProcess;
 
 Env::execute(); //executa cod class env
 
 $factoryProcessServerElement = new FactoryProcessServerElement();
 $factoryProcessServerElement->operation();
+
+RouteProcess::execute();
