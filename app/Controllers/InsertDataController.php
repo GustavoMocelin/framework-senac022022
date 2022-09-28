2b6ce2240345240fb5255fceb66410a8817916aa
GUSTAVO MOCELIN PROCOPIO <gustavo.9942@aluno.pr.senac.br>
2022-09-27 22:25:17 -0300

    aula html

--- app/Controllers/InsertDataController.php
+++ app/Controllers/InsertDataController.php
@@ -8,10 +8,31 @@ use App\FrameworkTools\Database\DatabaseConnection;
 class InsertDataController extends AbstractControllers{
 
     public function exec (){
+       
         $pdo = DatabaseConnection::start()->getPDO();
         //dd($pdo); 
         $params = $this->processServerElements->getInputJSONData();
        // dd($params);
+       $response = ['success'=>true];
+       $attrName;
+
+       try{
+       if(!$params['name'] ){
+            $attrName = 'name';
+            throw new \Exception('Digite o nome');
+        }
+
+        if(!$params['last_name'] ){
+            $attrName = 'last_name';
+            throw new \Exception('Digite o ultimo nome');
+        }
+
+        if(!$params['age'] ){
+            $attrName = 'age';
+            throw new \Exception('Digite a idade');
+        }
+
+
        $query = "INSERT INTO user (name,last_name,age) VALUES (:name,:last_name,:age)";
 
        $statement = $pdo->prepare($query);
@@ -22,10 +43,16 @@ class InsertDataController extends AbstractControllers{
             ':age' => $params["age"]
        ]);
 
-        view([
-            "sucess" => true         
-            
-        ]);
+    }catch(\Exception $e){
+        $response = [
+            'success' => false,
+            'message' => $e->getMessage(),
+            'missingAtribute' => $attrName
+        ];
+    }
+
+
+        view($response);
     }
 }
 
